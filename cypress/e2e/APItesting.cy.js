0edb0fb91bc3a70f6422df3629a95adb385a674c
Antonio Vitor de Mello <antonio.vitor.mello@everis.com>
2022-12-13 11:02:54 -0300

    tentativa de proteção de parametros key e token

--- cypress/e2e/APItesting.cy.js
+++ cypress/e2e/APItesting.cy.js
@@ -1,8 +1,8 @@
 describe('Interagir com Trello via API', () => {
     context('Teste', () => {
 
-        var key = "2c24cf0d8f75c2c205413a89e253b989";
-        var token = "97cc50a039dbcb7979fc455869485d6bc21768c38a98c0abfbf11ae13296c0c7";
+        var [key1, key2] = ["2c24cf0d8f75c2c2", "05413a89e253b989"];
+        var [token1, token2] = ["97cc50a039dbcb7979fc455869485d6b", "c21768c38a98c0abfbf11ae13296c0c7"];
 
         it('Cadastrar um board', () => {
             var boardName = "teste-board-Antonio";
@@ -11,7 +11,7 @@ describe('Interagir com Trello via API', () => {
             // Request POST para criação de board do zero
             cy.request({
                 method: 'POST',
-                url: `https://api.trello.com/1/boards/?name=${boardName}&key=${key}&token=${token}`,
+                url: `https://api.trello.com/1/boards/?name=${boardName}&key=${key1}${key2}&token=${token1}${token2}`,
             })
                 .its('body')
                 .should('deep.contain', {
@@ -27,7 +27,7 @@ describe('Interagir com Trello via API', () => {
             // Registrando alias para resgatar id do primeiro board ativo
             cy.request({
                 method: 'GET',
-                url: `https://api.trello.com/1/members/me/boards?&key=${key}&token=${token}`,
+                url: `https://api.trello.com/1/members/me/boards?&key=${key1}${key2}&token=${token1}${token2}`,
             }).as('getBoardId');
 
             // Utilizando alias para resgatar o id do primeiro board
@@ -38,7 +38,7 @@ describe('Interagir com Trello via API', () => {
                 // Registrando alias para resgatar id da primeira lista do primeiro board
                 cy.request({
                     method: 'GET',
-                    url: `https://api.trello.com/1/boards/${idBoard}/lists?key=${key}&token=${token}`,
+                    url: `https://api.trello.com/1/boards/${idBoard}/lists?key=${key1}${key2}&token=${token1}${token2}`,
                 }).as('getListId');
             });
 
@@ -50,7 +50,7 @@ describe('Interagir com Trello via API', () => {
                 // Execução da request de criação do card e validação da response
                 cy.request({
                     method: 'POST',
-                    url: `https://api.trello.com/1/cards?idList=${idList}&key=${key}&token=${token}`,
+                    url: `https://api.trello.com/1/cards?idList=${idList}&key=${key1}${key2}&token=${token1}${token2}`,
                     body: {
                         name: cardEntry,
                     }
@@ -68,7 +68,7 @@ describe('Interagir com Trello via API', () => {
             // Registrando alias para resgatar id do primeiro board ativo
             cy.request({
                 method: 'GET',
-                url: `https://api.trello.com/1/members/me/boards?&key=${key}&token=${token}`,
+                url: `https://api.trello.com/1/members/me/boards?&key=${key1}${key2}&token=${token1}${token2}`,
             }).as('getBoardId');
 
             // Utilizando alias para resgatar o id do primeiro board
@@ -79,7 +79,7 @@ describe('Interagir com Trello via API', () => {
                 // Registrando alias para resgatar id do primeiro card do primeiro board
                 cy.request({
                     method: 'GET',
-                    url: `https://api.trello.com/1/boards/${idBoard}/cards?key=${key}&token=${token}`,
+                    url: `https://api.trello.com/1/boards/${idBoard}/cards?key=${key1}${key2}&token=${token1}${token2}`,
                 }).as('getCardId');
             });
 
@@ -91,7 +91,7 @@ describe('Interagir com Trello via API', () => {
                 // Execução da request de deleção do card e validação do response
                 cy.request({
                     method: 'DELETE',
-                    url: `https://api.trello.com/1/cards/${idCard}?key=${key}&token=${token}`,
+                    url: `https://api.trello.com/1/cards/${idCard}?key=${key1}${key2}&token=${token1}${token2}`,
                 })
                     .its('body')
                     .should('deep.contain', {
@@ -101,7 +101,7 @@ describe('Interagir com Trello via API', () => {
                 // Validação de chamada pelo id do cartão para comprovar remoção
                 cy.request({
                     method: 'GET',
-                    url: `https://api.trello.com/1/cards/${idCard}?key=${key}&token=${token}`,
+                    url: `https://api.trello.com/1/cards/${idCard}?key=${key1}${key2}&token=${token1}${token2}`,
                     failOnStatusCode: false
                 })
                     .its('body')
@@ -116,7 +116,7 @@ describe('Interagir com Trello via API', () => {
             // Registrando alias para resgatar id do primeiro board ativo
             cy.request({
                 method: 'GET',
-                url: `https://api.trello.com/1/members/me/boards?&key=${key}&token=${token}`,
+                url: `https://api.trello.com/1/members/me/boards?&key=${key1}${key2}&token=${token1}${token2}`,
             }).as('getBoardId');
 
             // Utilizando alias para resgatar o id do primeiro board
@@ -126,7 +126,7 @@ describe('Interagir com Trello via API', () => {
                 // Execução da request de deleção do board e validação do response
                 cy.request({
                     method: 'DELETE',
-                    url: `https://api.trello.com/1/boards/${idBoard}?key=${key}&token=${token}`,
+                    url: `https://api.trello.com/1/boards/${idBoard}?key=${key1}${key2}&token=${token1}${token2}`,
                 })
                     .its('body')
                     .should('deep.contain', {
@@ -135,7 +135,7 @@ describe('Interagir com Trello via API', () => {
 
                 cy.request({
                     method: 'GET',
-                    url: `https://api.trello.com/1/members/me/boards?&key=${key}&token=${token}`,
+                    url: `https://api.trello.com/1/members/me/boards?&key=${key1}${key2}&token=${token1}${token2}`,
                     failOnStatusCode: false
                 })
                     .its('body')
